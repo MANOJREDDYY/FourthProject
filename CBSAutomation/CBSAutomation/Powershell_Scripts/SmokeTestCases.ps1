b644a84e778d4719d84eae509fd9a2ea7396e8fa
Manoj Reddy <Manoj.Reddy@efi.com>
2018-10-31 13:58:07 +0530

    Changed the result path

--- CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
+++ CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
@@ -7,19 +7,13 @@ $TestDLL="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAu
 
 $TestResult="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAutomation" +"\Reports\"
 Write-Host "starting Nunit Test"
-$Preqfullnamestr="CreateMaterial"
-
-
-$fullnamestr =$Preqfullnamestr | ForEach-Object{"CBSAutomation.TestScripts.$_,"}
-
-Write-Host $fullnamestr 
-
- $reportNunitDir="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\packages\ReportUnit.1.5.0-beta1\tools\ReportUnit.exe"
+$reportNunitDir="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\packages\ReportUnit.1.5.0-beta1\tools\ReportUnit.exe"
 $reportNunitInputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\xmlTestResults"
 $reportNunitOutputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\HtmlTestResults"
 $NumArray = ("CreateMaterial")
 Foreach ($Item in $NumArray) {
 &$NunitDir  $TestDLL --where class==CBSAutomation.TestScripts.Smoke_Test_Scripts.$Item --labels=off --work=$reportNunitInputpath --result="Project.SmokeCases.Test.xml;format=nunit3"
+&$reportNunitDir $reportNunitInputpath $reportNunitOutputpath 
 }
 
 
