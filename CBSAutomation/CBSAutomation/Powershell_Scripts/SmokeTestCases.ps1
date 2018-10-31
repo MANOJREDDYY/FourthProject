70690a1f52a46bd046cfdf5d34ab7b913961a26f
Manoj Reddy <Manoj.Reddy@efi.com>
2018-10-31 12:14:50 +0530

    Changed the path for c:

--- CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
+++ CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
@@ -1,11 +1,11 @@
 ﻿
-$NunitDir="C:\GitCBSPostgress\cti-pbs\DEV\NET4\SeleniumAutomation\CBSAutomation\CBSAutomation"+ "\Nunit\nunit3-console.exe"
+$NunitDir="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAutomation"+ "\Nunit\nunit3-console.exe"
 
                            
 $extentReportDir=$currentDir + "\ExtentReports\"
-$TestDLL="C:\GitCBSPostgress\cti-pbs\DEV\NET4\SeleniumAutomation\CBSAutomation\CBSAutomation" +"\bin\Debug\CBSAutomation.dll"
+$TestDLL="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAutomation" +"\bin\Debug\CBSAutomation.dll"
 
-$TestResult="C:\GitCBSPostgress\cti-pbs\DEV\NET4\SeleniumAutomation\CBSAutomation\CBSAutomation" +"\Reports\"
+$TestResult="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAutomation" +"\Reports\"
 Write-Host "starting Nunit Test"
 $Preqfullnamestr="CreateCustomer"
 
@@ -14,9 +14,9 @@ $Preqfullnamestr="CreateCustomer"
 
 
 
- $reportNunitDir="C:\GitCBSPostgress\cti-pbs\DEV\NET4\SeleniumAutomation\CBSAutomation\packages\ReportUnit.1.5.0-beta1\tools\ReportUnit.exe"
-$reportNunitInputpath ="C:\GitCBSPostgress\cti-pbs\DEV\NET4\SeleniumAutomation\CBSAutomation\xmlTestResults"
-$reportNunitOutputpath ="C:\GitCBSPostgress\cti-pbs\DEV\NET4\SeleniumAutomation\CBSAutomation\HtmlTestResults"
+ $reportNunitDir="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\packages\ReportUnit.1.5.0-beta1\tools\ReportUnit.exe"
+$reportNunitInputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\xmlTestResults"
+$reportNunitOutputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\HtmlTestResults"
 
 &$NunitDir  $TestDLL --where namespace==CBSAutomation.TestScripts.Smoke_Test_Scripts  --labels=off --work=$reportNunitInputpath --result="Project.SmokeCases.Test.xml;format=nunit3" 
 
