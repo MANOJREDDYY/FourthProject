7e945de7fc2096ba1229d8af472c5461356788cd
Manoj Reddy <Manoj.Reddy@efi.com>
2018-10-31 12:21:17 +0530

    Changes power shell script for each loop

--- CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
+++ CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
@@ -7,19 +7,20 @@ $TestDLL="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAu
 
 $TestResult="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\CBSAutomation" +"\Reports\"
 Write-Host "starting Nunit Test"
-$Preqfullnamestr="CreateCustomer"
+$Preqfullnamestr="CreateMaterial"
 
 
-#$fullnamestr =$Preqfullnamestr | ForEach-Object{"CBSAutomation.TestScripts.$_,"}
+$fullnamestr =$Preqfullnamestr | ForEach-Object{"CBSAutomation.TestScripts.$_,"}
 
-
+Write-Host $fullnamestr 
 
  $reportNunitDir="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\packages\ReportUnit.1.5.0-beta1\tools\ReportUnit.exe"
 $reportNunitInputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\xmlTestResults"
 $reportNunitOutputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\HtmlTestResults"
-
-&$NunitDir  $TestDLL --where namespace==CBSAutomation.TestScripts.Smoke_Test_Scripts  --labels=off --work=$reportNunitInputpath --result="Project.SmokeCases.Test.xml;format=nunit3" 
-
+$NumArray = ("CreateMaterial")
+Foreach ($Item in $NumArray) {
+&$NunitDir  $TestDLL --where class==CBSAutomation.TestScripts.Smoke_Test_Scripts.$Item --labels=off --work=$reportNunitInputpath --result="Project.SmokeCases.Test.xml;format=nunit3"
+}
 
 
 &$reportNunitDir $reportNunitInputpath $reportNunitOutputpath 
