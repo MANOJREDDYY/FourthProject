a822a509082ff0326ca16aa7328d5d05c5ac7af4
Manoj Reddy <Manoj.Reddy@efi.com>
2018-10-31 14:36:06 +0530

    new class create customer added for execution

--- CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
+++ CBSAutomation/CBSAutomation/Powershell_Scripts/SmokeTestCases.ps1
@@ -10,7 +10,7 @@ Write-Host "starting Nunit Test"
 $reportNunitDir="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\packages\ReportUnit.1.5.0-beta1\tools\ReportUnit.exe"
 $reportNunitInputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\xmlTestResults"
 $reportNunitOutputpath ="C:\Users\manojred\.jenkins\workspace\FourthProject\CBSAutomation\HtmlTestResults"
-$NumArray = ("CreateMaterial")
+$NumArray = ("CreateMaterial","CreateCustomer")
 Foreach ($Item in $NumArray) {
 &$NunitDir  $TestDLL --where class==CBSAutomation.TestScripts.Smoke_Test_Scripts.$Item --labels=off --work=$reportNunitInputpath --result="Project.SmokeCases.Test.xml;format=nunit3"
 &$reportNunitDir $reportNunitInputpath $reportNunitOutputpath 
